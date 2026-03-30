3683e2a303e0c9cbc4e2c8e959bff9b9b37365f8
withering_snow <1837137029@qq.com>
2026-03-30 09:00:03 +0800

    注释添加，床位管理更新

--- include/repository/data_manager.h
+++ include/repository/data_manager.h
@@ -5,14 +5,46 @@
 #include <HIS_entity.h>
 
 // 生命周期：全部实体链表构建与释放
+/**
+ * @brief 初始化所有数据链表
+ */
 void Data_init();
+
+/**
+ * @brief 销毁所有数据链表
+ */
 void Data_destroy();
 
+
 // 获取对应链表
+/**
+ * @brief 获取doctor链表
+ * @return 返回doctor数据链表的头节点
+ */
 List_T Data_get_doctor();
+
+/**
+ * @brief 获取fund链表
+ * @return 返回fund数据链表的头节点
+ */
 List_T Data_get_fund();
+
+/**
+ * @brief 获取medicine链表
+ * @return 返回medicine数据链表的头节点
+ */
 List_T Data_get_medicine();
+
+/**
+ * @brief 获取patient链表
+ * @return 返回patient数据链表的头节点
+ */
 List_T Data_get_patient();
+
+/**
+ * @brief 获取ward链表
+ * @return 返回ward数据链表的头节点
+ */
 List_T Data_get_ward();
 
 #endif// HIS_DATA_MANAGER_H
