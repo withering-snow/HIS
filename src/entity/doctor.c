77315487b3608eb7422d47a11e4703e0ed3d3cc7
legend-has-never-gone <13903952002@163.com>
2026-04-07 14:26:23 +0800

    doctor.c第二次修改完成

--- src/entity/doctor.c
+++ src/entity/doctor.c
@@ -40,12 +40,14 @@ bool Doctor_get_sex(Doctor_T d) {
          printf("Doctor's sex is man");
      }
      else printf("Doctor's sex is woman");
+    return 0;
 }
 bool Doctor_is_active(Doctor_T d) {
     if(d->is_active!=0) {
         printf("Doctor is active");
     }
     else printf("Doctor isn't active");
+    return 0;
 }
 Department Doctor_get_department(Doctor_T d) {
     printf("Doctor's department is");
@@ -63,35 +65,38 @@ Status Doctor_set_id(Doctor_T d, int new_id) {
     d->id=new_id;
     printf("Doctor's new id is");
     printf("%d\n", new_id);
-    return new_id;
+    return HIS_OK;
 }
 Status Doctor_set_age(Doctor_T d, int new_age) {
     d->age=new_age;
     printf("Doctor's new age is");
     printf("%d\n", new_age);
-    return new_age;
+    return HIS_OK;
 }
 Status Doctor_set_sex(Doctor_T d, bool new_sex) {
     d->sex=new_sex;
     printf("Doctor's new sex is ");
     if(new_sex!=0) printf("man");
     else printf("woman");
+    return HIS_OK;
 }
 Status Doctor_set_is_active(Doctor_T d, bool new_is_active) {
     d->is_active=new_is_active;
     if(new_is_active!=0) printf("Doctor is active");
     else printf("Doctor isn't active");
+    return HIS_OK;
 }
 Status Doctor_set_department(Doctor_T d, Department new_department) {
     d->department=new_department;
     printf("Doctor's new department is");
     printf("%d\n", new_department);
-    return new_department;
+    return HIS_OK;
 }
 Status Doctor_set_name(Doctor_T d, const char* new_name) {
     strcpy(d->name, new_name);
     printf("Doctor's new name is");
     printf("%s\n", new_name);
+    return HIS_OK;
 }
 
 // 比较器
