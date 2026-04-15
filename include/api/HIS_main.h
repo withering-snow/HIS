419bf99fc9144d65fdcd79e4b9815843f0d50735
withering_snow <1837137029@qq.com>
2026-04-15 08:19:56 +0800

    [CRITICAL]struct fix for io&&db

--- include/api/HIS_main.h
+++ include/api/HIS_main.h
@@ -2,8 +2,9 @@
 #define HIS_HIS_MAIN_H
 
 #include <HIS_core.h>
-#include <HIS_ctrl.h>
+#include <HIS_db.h>
 #include <HIS_entity.h>
+#include <HIS_io.h>
 #include <HIS_service.h>
 #include <HIS_ui.h>
 
