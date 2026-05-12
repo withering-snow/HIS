2296a3eef4f65f70ba8f7c5c4ecc48b3e02fbccb
9uiij <2158448865@qq.com>
2026-05-12 16:24:19 +0800

    log finished

--- include/ctrl/log_ctrl.h
+++ include/ctrl/log_ctrl.h
@@ -1,4 +1,12 @@
 #ifndef HIS_LOG_CTRL_H
 #define HIS_LOG_CTRL_H
 
+#include <HIS_core.h>
+#include <HIS_entity.h>
+#include <stdarg.h>
+
+void Log_open();
+void Log_close();
+void Log_printf(AccountClass class,long long id,const char* format, ...);
+
 #endif //HIS_LOG_CTRL_H
